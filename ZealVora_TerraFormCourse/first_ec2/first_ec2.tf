ba537c3ddf79b2c7c20dbec8236c053608a8891b
Gergo Kiss <gkiss@cyansecurity.com>
2023-09-20 16:14:10 +0200

    started new course on Udemy

---
+++ ZealVora_TerraFormCourse/first_ec2/first_ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region     = "eu-central-1"
+}
+
+resource "aws_instance" "myec2" {
+    ami = "ami-0b9094fa2b07038b8"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "my-first-ec2"
+    }
+}
