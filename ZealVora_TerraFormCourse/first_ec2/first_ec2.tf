f9aa0d198c1e343782538bedce9bf7883189d71b
Gergo Kiss <gkiss@cyansecurity.com>
2023-09-26 14:25:43 +0200

    updated aws-terraform version

--- ZealVora_TerraFormCourse/first_ec2/first_ec2.tf
+++ ZealVora_TerraFormCourse/first_ec2/first_ec2.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0" #csak olyan verziót használ ami 5.X-es
+    }
+  }
+}
+
 provider "aws" {
   region     = "eu-central-1"
 }
