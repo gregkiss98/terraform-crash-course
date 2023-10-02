5f657cb6213ff6902300708374a4bae7d0a6a149
Kiss Gergő <kissge@gain.uni-sopron.hu>
2023-10-02 16:28:09 +0200

    new lesson: reference attributes

---
+++ ZealVora_TerraFormCourse/reference-attributes/reference-attributes.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region     = "eu-central-1"
+}
+
+//create Elastic IP
+resource "aws_eip" "myEIP" {
+  domain = "vpc"
+}
+
+//create a Security Group
+resource "aws_security_group" "mySecurityGroup" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks = [ "${aws_eip.myEIP.public_ip}/32" ] //reference myEIP public address. It needs '"{}"/32' because it would throw an error.
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
