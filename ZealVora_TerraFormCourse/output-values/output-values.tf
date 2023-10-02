918135de957f15eb91104cfafeaa3c8dc32e1768
Kiss Gergő <kissge@gain.uni-sopron.hu>
2023-10-02 16:42:09 +0200

    new lesson: output-values

---
+++ ZealVora_TerraFormCourse/output-values/output-values.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region     = "eu-central-1"
+}
+
+
+//create Elastic IP
+resource "aws_eip" "myEIP" {
+  domain = "vpc"
+}
+
+//output public IP from the created EIP
+output "myEIP_Info" {
+  //value = aws_eip.myEIP.public_ip
+
+  //we can customize the output
+  value = "https://${aws_eip.myEIP.public_ip}:8080"
+  //value can be used in different projects
+}
