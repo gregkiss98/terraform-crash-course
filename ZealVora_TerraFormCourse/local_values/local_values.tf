1ed8f139adb0ab8f546a54c72a1285e3133b04d4
Kiss Gergő <kissge@gain.uni-sopron.hu>
2023-10-08 10:04:51 +0200

    new lesson added: local values

---
+++ ZealVora_TerraFormCourse/local_values/local_values.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region     = "eu-central-1"
+}
+
+//local values can help to avoid repeating the same values or expressions multiple times
+locals {
+  common_tags = {
+    Owner = "DevOps Team"
+    Service = "backend"
+  }
+}
+
+resource "aws_instance" "dev" {
+    ami = "ami-0b9094fa2app-b07038b8"
+    instance_type = "t2.micro"
+    tags = local.common_tags
+}
+
+resource "aws_instance" "db-dev" {
+    ami = "ami-0b9094fa2b07038b8"
+    instance_type = "t2.micro"
+    tags = local.common_tags
+
+}
+
+resource "aws_ebs_volume" "db-ebs" {
+    availability_zone = "eu-central-1"
+    size = 8
+    tags = local.common_tags
+}
