7abe7a006e1642771c096ab5a8e224ca275c053c
Kiss Gergő <kissge@gain.uni-sopron.hu>
2023-10-09 10:36:58 +0200

    lesson added: dynamic blocks

---
+++ ZealVora_TerraFormCourse/dynamic-blocks/dynamic-blocks.tf
@@ -0,0 +1,31 @@
+variable "sg_ports" {
+  type        = list(number)
+  description = "list of ingress ports"
+  default     = [8200, 8201,8300, 9200, 9500]
+}
+
+resource "aws_security_group" "dynamicsg" {
+  name        = "dynamic-sg"
+  description = "Ingress for Vault"
+
+  dynamic "ingress" {
+    for_each = var.sg_ports
+    iterator = port //optional value, sets the name of a temporary variable that reperesent the current element of the complex value
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  dynamic "egress" {
+    for_each = var.sg_ports
+    content {
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
