a4275dc683b6a9c15ff278d3a2e96c2f16cae4a9
Gergo Kiss <gkiss@cyansecurity.com>
2023-10-18 15:29:43 +0200

    added new lessons

---
+++ ZealVora_TerraFormCourse/dealing-with-large-infra.tf
@@ -0,0 +1,8 @@
+/*
+
+when you have a large infrastructure, you will face issues realted to API limits of the provider
+-> you switch to smaller configs where each can be applied indepententl
+when you already did that, and still not enough you can -target=resource to specify resource
+or you can turn off -refresh=false, so in the beginning it won't check already planned configs
+
+*/
