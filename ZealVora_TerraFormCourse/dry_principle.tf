757c3e70626891d395414091faf6f58d61f012f0
Gergo Kiss <gkiss@cyansecurity.com>
2023-10-19 14:41:36 +0200

    new lesson added: dry principle

---
+++ ZealVora_TerraFormCourse/dry_principle.tf
@@ -0,0 +1,7 @@
+/*
+
+DRY = Don't Repeat Yourself is a principe of software development aimet at reducing repetiotion of software patterns.
+We can centralize terraform resources and call out from the TF files whenever required.
+We can use so called 'modules'.
+
+*/
