c36ea13d876a4a14708b22f30bc63a39aefb67b7
Gergo Kiss <gkiss@cyansecurity.com>
2023-11-10 14:28:23 +0100

    lesson adedd: terraform registry

---
+++ ZealVora_TerraFormCourse/terraform_registry.tf
@@ -0,0 +1,15 @@
+/*
+
+Terraform Registry is repository of modules written by the Terraform community.
+You can find verified modules that are maintained by third parties. Veryfied modules are reviewed by HashiCorp and actively maintained. 
+Blue verification badge notes the maintained modules.
+
+How to publish a module:
+Published modules support versioning, automatically generate documentation, allow browsing version histories, show examples and READMEs etc.
+- module must be in Github and a public repo.
+- must be named in a three-part format: terraform-<PROVIDER>-<NAME>
+- GitHub repo descr. is used to poulate the short descr. of the module
+- the module must adhere the standard module structure
+- the registry uses tags to identify module versions. Release tag names must be a semantic version, which can optionally be prefixed with a ".v"
+
+*/
